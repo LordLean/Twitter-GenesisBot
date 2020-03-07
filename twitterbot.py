f7beccc7a593b6500baf196a5fcd60193b01e6cb
Alex Holder <59891036+LordLean@users.noreply.github.com>
2020-03-07 19:57:51 +0000

    Add files via upload

--- twitterbot.py
+++ twitterbot.py
@@ -1,18 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import sys
-sys.path.append(r'/Users/alex/Documents/Coding_Projects/twitterbot')
+sys.path.append("/Users/alex/Documents/Coding_Projects/twitterbot/")
+import tweepy, time
 import random
-from genesiswords import genesis_words
-from nltk.tokenize import sent_tokenize
+from genesisList import genesis_sentences
+from config import keys as k
 
-def get_quote(words):
+def get_quote(sents):
 
-    sents = sent_tokenize(words)
     rand_index = random.randint(0,len(sents) - 1)
-    curr_quote = sents[rand_index] + sents[rand_index+1]
+    curr_quote = sents[rand_index]# + sents[rand_index+1]
 
     return curr_quote + "\n Brought to you by Genesis_Bot : Praise Be!"
 
-print(get_quote(genesis_words))
-
 if __name__ == "__main__":
-    get_quote(genesis_words)
+    get_quote(genesis_sentences)
+
+auth = tweepy.OAuthHandler(k["CONSUMER_KEY"], k["CONSUMER_SECRET"])
+auth.set_access_token(k["ACCESS_KEY"], k["ACCESS_SECRET"])
+api = tweepy.API(auth)
+
+while True:
+    api.update_status(get_quote(genesis_sentences))
+    time.sleep(43200) #Generates twice daily
